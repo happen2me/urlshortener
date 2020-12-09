c3acd2a45680804f6a53c6e2b262330180fc7f23
yuanchun <immr.shen@gmail.com>
2020-12-09 17:58:34 +0800

    can handle basic url save and read now

--- src/main/java/co/yuanchun/app/App.java
+++ src/main/java/co/yuanchun/app/App.java
@@ -1,10 +1,16 @@
 package co.yuanchun.app;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.Reader;
 import java.net.InetSocketAddress;
+import java.sql.SQLException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
+
+import javax.swing.plaf.synth.SynthStyleFactory;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -15,45 +21,82 @@ import com.sun.net.httpserver.HttpServer;
  *
  */
 public class App {
-    public static void main(String[] args) throws IOException
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Initializing databse");
+
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-        server.createContext("/test", new MyHttpHandler());
-        ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor)Executors.newFixedThreadPool(10);
+        server.createContext("/", new MyHttpHandler());
+        ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
         server.setExecutor(threadPoolExecutor);
         server.start();
     }
 
-    private static class MyHttpHandler implements HttpHandler{
+    private static class MyHttpHandler implements HttpHandler {
+        UrlShortener shortener;
+
+        public MyHttpHandler() {
+            shortener = new UrlShortener();
+        }
 
         @Override
         public void handle(HttpExchange exchange) throws IOException {
-            String requstParams = null;
+            String requestParams = null;
             if("GET".equals(exchange.getRequestMethod())){
-                requstParams = handleGetRequest(exchange);
+                requestParams = handleGetRequest(exchange);
             }
             else if("POST".equals(exchange.getRequestMethod())){
-                requstParams = handlePostRequest(exchange);
+                requestParams = handlePostRequest(exchange);
             }
+            handleResponse(exchange, requestParams);
         }
 
         private String handleGetRequest(HttpExchange exchange){
-            return exchange.getRequestURI()
-                .toString()
-                .split("\\?")[1]
-                .split("=")[1];
+            String query = exchange.getRequestURI().getQuery().toString();
+            System.out.println("Get query: " + query);
+            String urlFound = shortener.findAlias(query);
+            if(urlFound == ""){
+                System.out.println("Queried alias not found.");
+            }
+            return urlFound;
         }
 
         private String handlePostRequest(HttpExchange exchange){
-            return exchange.getRequestBody().toString();
+            String requestString = null;
+            try (InputStream requestStream = exchange.getRequestBody();) {
+                requestString = tranlateInputStream(requestStream);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return "";
+            }
+            System.out.print("Request body is:");
+            System.out.println(requestString);
+            String alias = shortener.insertUrl(requestString, null);
+            return alias;
+        }
+
+        private String tranlateInputStream(InputStream in){
+            final StringBuilder sBuilder = new StringBuilder();
+            final char[] buffer = new char[1024];
+            try (Reader r = new InputStreamReader(in, "UTF-8")) {
+                int len;
+                while ((len = r.read(buffer)) != -1) {
+                 sBuilder.append(buffer, 0, len);   
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            String output = sBuilder.toString().stripTrailing();
+            return output;
         }
         
-        private void handleResponse(HttpExchange exchange, String requestParams){
+        private void handleResponse(HttpExchange exchange, String response) throws IOException {
             OutputStream outputStream = exchange.getResponseBody();
-
+            exchange.sendResponseHeaders(200, response.length());
+            outputStream.write(response.getBytes());
+            outputStream.flush();
+            outputStream.close();
         }
-
 
     }
 }
